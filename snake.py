b218fcb3daaa8ad570045ac193007dc8cdc1d931
jsay <pr.juliya@gmail.com>
2016-04-27 23:03:54 +0300

    drawing point class added, func added

--- snake.py
+++ snake.py
@@ -6,11 +6,20 @@ import os
 import turtle
 from tkinter import *
 
-x1 = 30
+x1 = 20
 y1 = 30
 sym1 = '*'
 
-def draw(x,y,sym):
-	print("\033[%d;%dH%s" % (x, y, sym))
 
-draw(10, 20, '#')
+class Point(object):
+	def __init__(self, x = 0, y = 0, sym = "*"):
+		self.x = x
+		self.y = y
+		self.sym = sym
+	def draw(self):
+		print("\033[%d;%dH%s" % (self.x, self.y, self.sym))
+
+p1 = Point(10,20,';')
+p2 = Point()
+p1.draw()
+p2.draw()
