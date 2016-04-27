e6b5b221e78b7b9d224cdb1ffe5f1bc1beeddf26
jsay <pr.juliya@gmail.com>
2016-04-27 16:26:04 +0300

    lesson 2 changes (drawing a symbol in a procedure style)

--- snake.py
+++ snake.py
@@ -3,6 +3,14 @@
 __author__='julia sayapina'
 
 import os
+import turtle
+from tkinter import *
 
-print ("hello world")
-input('press any key')
+x1 = 30
+y1 = 30
+sym1 = '*'
+
+def draw(x,y,sym):
+	print("\033[%d;%dH%s" % (x, y, sym))
+
+draw(10, 20, '#')
