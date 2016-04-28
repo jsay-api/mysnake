c9f81a7296046121756ef2b2d58136d6bbd119fd
jsay <pr.juliya@gmail.com>
2016-04-28 16:21:39 +0300

    Figure base class added, self.plist fixed. Note: when inheriting from the base class and call the var x from it, still call it like self.x even in child class!

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 #coding: utf-8
 __author__='julia sayapina'
 
@@ -9,6 +8,23 @@ from tkinter import *
 x1 = 20
 y1 = 30
 sym1 = '*'
+
+# #creating canvas
+# root = Tk()
+# #setting the canvas' name
+# root.title("Snake game")
+# #launching the canvas
+# root.mainloop()
+
+# #canvas sizes vars
+# WIDTH = 800
+# HEIGHT = 600
+
+# # создаем экземпляр класса Canvas (его мы еще будем использовать) и заливаем все зеленым цветом
+# c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#003300")
+# c.grid()
+# # Наводим фокус на Canvas, чтобы мы могли ловить нажатия клавиш
+# c.focus_set()
 
 
 class Point(object):
@@ -24,38 +40,48 @@ p2 = Point()
 # p1.draw()
 # p2.draw()
 
-class HorizontalLine(object):
-	
+class Figure(object):
+	# def __init__(self):
+	# 	self.plist = []
+	plist = [] 
+	def ldraw(self):
+		for i in self.plist: 
+			i.draw()
+
+
+class HorizontalLine(Figure):
 	def __init__(self, xleft, xright, y = 0, symb = "*"):
+		super().__init__()
 		self.xleft = xleft
 		self.xright = xright
 		self.y = y
 		self.symb = symb
-	def ldraw(self):	
-		plist = []
+
+	def pappend(self):
 		for i in range(self.xleft, self.xright):
 			p = Point(i, self.y, self.symb)
-			plist.append(p)
-		for i in plist: 
-			i.draw()
+			self.plist.append(p)
+		return self.plist
+		
 
-class VerticalLine(object):
-	
+class VerticalLine(Figure):
 	def __init__(self, x, yup, ybottom, symb = "*"):
+		super().__init__()
+		#Figure.__init__(self)
 		self.x = x
 		self.yup = yup
 		self.ybottom = ybottom
 		self.symb = symb
-	def ldraw(self):	
-		plist = []
+	
+	def pappend(self):	
 		for i in range(self.yup, self.ybottom):
 			p = Point(self.x, i, self.symb)
-			plist.append(p)
-		for i in plist: 
-			i.draw()
+			self.plist.append(p)
+		return self.plist
+
 
 h1 = HorizontalLine(3,10,20,"+")
-h1.ldraw()	
+h1.pappend(), h1.ldraw()	
 h2 = VerticalLine(3, 20, 25, "-")
-h2.ldraw()
+h2.pappend(), h2.ldraw()
 
