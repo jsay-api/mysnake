b578dba5e3f239e9d0cb84b494f59db3a27ec6d3
jsay <pr.juliya@gmail.com>
2016-05-03 12:42:00 +0300

    getch cannot be used with threads, so termios is back, gonna try threading (reverted from commit 916aa6ae20a5570a9066997c1d211d24bb71f27d)

--- snake.py
+++ snake.py
@@ -8,29 +8,6 @@ from time import sleep
 import sys,tty,termios
 import getch
 
-"""For control use keys:
-s - left
-d - right
-e - up
-x - down
-q - escape
-any other key – game over"""
-
-
-try : # on windows
-    from msvcrt import getch
-except ImportError : # on unix like systems
-    import sys, tty, termios
-    def getch() :
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try :
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally :
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
 
 class Point(object):
     def __init__(self, x = 0, y = 0, sym = "*"):
@@ -133,30 +110,25 @@ class Snake(Figure):
         head.draw()
 
     def get(self):
-        while True:
-            key = getch()
-            if key == "e":
-                self.direction = Direction.UP
-                #sleep(0.1)
-                break
-            elif key == "s":
-                self.direction = Direction.LEFT
-                #sleep(0.1)
-                break
-            elif key == "x":
-                self.direction = Direction.DOWN
-                #sleep(0.1)
-                break
-            elif key == "d":
-                self.direction = Direction.RIGHT
-                #sleep(0.1)
-                break
-            elif key == "q":
-                print ("Exit")
-                sys.exit(0)
-        # else: 
-        #     print ("Game over!")
-        #     sys.exit(0)
+        key = getch.getch()
+        if key == "e":
+            self.direction = Direction.UP
+            sleep(0.1)
+        elif key == "s":
+            self.direction = Direction.LEFT
+            sleep(0.1)
+        elif key == "x":
+            self.direction = Direction.DOWN
+            sleep(0.1)
+        elif key == "d":
+            self.direction = Direction.RIGHT
+            sleep(0.1)
+        elif key == "q":
+            print ("Exit")
+            sys.exit(0)
+        else: 
+            print ("Game over!")
+            sys.exit(0)
 
 
 
@@ -173,7 +145,7 @@ class Direction(object):
 
 
 def main():
-    os.system("clear")
+
     x1 = 20
     y1 = 30
     sym1 = '*'
@@ -185,16 +157,10 @@ def main():
     h2.pappend()
     snake = Snake(p1, 4, Direction.LEFT)
     snake.position()
-    #global key
-    
-    
     
     while True:
-        snake.move()
-        sleep(0.1)
         snake.get()
-        
-        
+        snake.move()
     
 
 
